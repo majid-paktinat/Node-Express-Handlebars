1dc5cc672f3992a276a7aee5673a1598c05550af
majid <majid.paktinat@gmail.com>
2020-03-08 13:54:05 -0400

    connection to heroku established

--- config/connection.js
+++ config/connection.js
@@ -6,15 +6,17 @@
 var mysql = require("mysql");
 
 // Setting up our connection information
-var source = {
-  localhost: {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "12345678",
-    database: "burgers_db"
-  }
-};
+let source;
+source = (process.env.JAWSDB_URL) ? process.env.JAWSDB_URL : {
+                                                              localhost: {
+                                                                host: "localhost",
+                                                                port: 3306,
+                                                                user: "root",
+                                                                password: "12345678",
+                                                                database: "burgers_db"
+                                                              }
+                                                            }
+
 
 
 // Creating our connection
